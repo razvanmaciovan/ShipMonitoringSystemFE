91999f61fca052fcca10aea1d8f97a53907c5d8f
Maciovan Razvan <razvan.maciovan02@e-uvt.ro>
2022-04-05 21:55:32 +0300

    Final Build

--- src/app/port/show-port/show-port.component.ts
+++ src/app/port/show-port/show-port.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { PortApiService } from 'src/app/port-api.service';
+import { Pipe, PipeTransform } from '@angular/core';
+export interface IPort{
+  portId:number;
+  portName:string;
+  portCountryId:number;
+}
 @Component({
   selector: 'app-show-port',
   templateUrl: './show-port.component.html',
   styleUrls: ['./show-port.component.css']
 })
-export class ShowPortComponent implements OnInit {
-  portList$!: Observable<any[]>;
 
+export class ShowPortComponent implements OnInit {
+  portList$!: Observable<IPort[]>;
+  countryList$!: Observable<IPort[]>;
 
   constructor(private service: PortApiService) { }
+  
 
   ngOnInit(): void {
     this.portList$ = this.service.getPortList();
+    this.countryList$ = this.service.getCountryList();
   }
 
 }
